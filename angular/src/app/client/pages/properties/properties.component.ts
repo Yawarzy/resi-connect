ebdcab6912e2bb6fca41e1cbca35754fcd735a43
Yawar Bhat <yawarbhatzy@gmail.com>
2023-07-28 21:12:43 +0530

    finished the homepage (#2)

--- angular/src/app/client/pages/properties/properties.component.ts
+++ angular/src/app/client/pages/properties/properties.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-property',
+  selector: 'app-properties',
   templateUrl: './properties.component.html',
   styleUrls: ['./properties.component.scss']
 })
--- angular/src/app/shared/shared.module.ts
+++ angular/src/app/shared/shared.module.ts
@@ -4,21 +4,27 @@ import {NavbarComponent} from './components/navbar/navbar.component';
 import {PageComponent} from './components/page/page.component';
 import {FooterComponent} from './components/footer/footer.component';
 import {RouterLinkWithHref} from "@angular/router";
+import {SelectFieldComponent} from './components/form/select-field/select-field.component';
+import {NzSelectModule} from "ng-zorro-antd/select";
+import {FormsModule} from "@angular/forms";
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
     PageComponent,
-    FooterComponent
+    FooterComponent,
+    SelectFieldComponent
   ],
   exports: [
-    NavbarComponent,
-    PageComponent
+    PageComponent,
+    SelectFieldComponent
   ],
   imports: [
     CommonModule,
-    RouterLinkWithHref
+    RouterLinkWithHref,
+    NzSelectModule,
+    FormsModule
   ]
 })
 export class SharedModule {
